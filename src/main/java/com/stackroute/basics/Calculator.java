5b1e70725ae9a649942a42894853455f46ad9ba6
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-02-12 15:45:24 +0530

    completed

--- src/main/java/com/stackroute/basics/Calculator.java
+++ src/main/java/com/stackroute/basics/Calculator.java
@@ -8,16 +8,61 @@ public class Calculator {
     // define,declare scanner and call getValues with scanner as parameter
     public static void main(String[] args) {
         scan = new Scanner(System.in);
-        new Calculator().getValues(scan);
+        char choice;
+        do{
+            new Calculator().getValues(scan);
+            System.out.println("Do you want to try again(y/n)");
+            choice = scan.next().charAt(0);
+
+        }while(choice!='n');
     }
 
     //Get values and which operator from the menu
     public void getValues(Scanner scan) {
-
+        System.out.println("Enter the first number:");
+        int firstNumber = scan.nextInt();
+        System.out.println("Enter the second number:");
+        int secondNumber = scan.nextInt();
+        System.out.println("Enter number beside the operation to perform: \n" +
+                " 1. Add \n" +
+                " 2. Subtract\n" +
+                " 3. Multiply\n" +
+                " 4. Divide");
+        int operator = scan.nextInt();
+        calculate(firstNumber, secondNumber, operator);
+        System.out.println(calculate(firstNumber, secondNumber, operator));
     }
 
     //perform operation based on the chosen switch case corresponding to the menu and return string
     public String calculate(int firstValue, int secondValue, int operator) {
-        return null;
+        String output;
+
+        switch (operator) {
+            case 1: {
+                output = firstValue + " + " + secondValue + " = " + (firstValue + secondValue);
+                break;
+            }
+            case 2: {
+                output = firstValue + " - " + secondValue + " = " + (firstValue - secondValue);
+                break;
+            }
+            case 3: {
+                output = firstValue + " * " + secondValue + " = " + (firstValue * secondValue);
+                break;
+            }
+            case 4: {
+                if (secondValue == 0) {
+                    output = "The divider (secondValue) cannot be zero";
+                    break;
+                }
+                output = firstValue + " / " + secondValue + " = " + (firstValue / secondValue);
+                break;
+            }
+            default: {
+                output = "Entered wrong option " + operator;
+                break;
+            }
+        }
+        return output;
     }
 }
